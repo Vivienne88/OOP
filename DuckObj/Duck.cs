28f58d30a4088c885c29327800e09f854b944390
vivi88 <babobigi@naver.com>
2019-01-08 00:16:16 +0900

    Design Pattern 1

---
+++ DuckObj/Duck.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    public abstract class Duck
+    {
+        protected FlyBehavior flyBehavior;
+        protected QuackBehavior quackBehavior;
+
+        public Duck()
+        {
+
+        }
+
+        public abstract void display();
+
+        public void performFly()
+        {
+            flyBehavior.Fly();
+        }
+
+        public void perfomQuack()
+        {
+            quackBehavior.Quack();
+        }
+
+        public void setFlyBehavior(FlyBehavior fb)
+        {
+            flyBehavior = fb;
+        }
+
+        public void setQuackBehaive(QuackBehavior qb)
+        {
+            quackBehavior = qb;
+        }
+
+        public void swim()
+        {
+            Console.WriteLine("All ducks float, even decoys!");
+        }
+    }
+}
